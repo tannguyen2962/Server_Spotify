16e903750f602021a1b4415e2965c306003fef13
tannguyen2962@gmail.com <tannguyen2962@gmail.com>
2022-03-28 16:01:36 +0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 // const url = "mongodb+srv://tannguyen:tannguyen123@cluster0.bxrvl.mongodb.net/SpotifyDB?retryWrites=true&w=majority"
 const url = "mongodb://localhost:27017/Spotify";
+
 const useMethodUser = require("./routes/method_user");
 const useMethodSong = require("./routes/method_song");
 const useMethodPlaylist = require("./routes/method_playlist");
@@ -13,7 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(url);
+mongoose.connect(url),
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
 app.use(useMethodSong);
 app.use(useMethodUser);
 app.use(useMethodPlaylist);
