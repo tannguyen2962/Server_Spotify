837f993d002f11a7d5ffaa6c1f4cb2911b336074
tannguyen2962@gmail.com <tannguyen2962@gmail.com>
2022-03-29 08:18:37 +0700

    change server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const url =
-  "mongodb+srv://tannguyen:tannguyen123@cluster0.xv4bw.mongodb.net/Server_Spotify?retryWrites=true&w=majority";
+// const url =
+//   "mongodb+srv://tannguyen:tannguyen123@cluster0.xv4bw.mongodb.net/Server_Spotify?retryWrites=true&w=majority";
 // const url = "mongodb://localhost:127.0.0.1/Spotify";
 
 const useMethodUser = require("./routes/method_user");
@@ -15,7 +15,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(url);
+mongoose
+  .connect(
+    "mongodb+srv://tannguyen:tannguyen123@cluster0.xv4bw.mongodb.net/Server_Spotify?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
 
 app.use(useMethodSong);
 app.use(useMethodUser);
